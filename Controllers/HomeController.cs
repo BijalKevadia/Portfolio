de4faf08f59bf22f39c09e0c6afabd25f779ad0f
Bijal Kevadia <kevadia.bijal@gmail.com>
2019-07-07 03:13:08 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bijalPortfolio.Models;
+
+namespace bijalPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Projects")]
+        [HttpGet]
+        public IActionResult Projects () {
+            return View();
+        }
+
+        public IActionResult Contact () {
+            return View();
+        }
+        
+        public IActionResult Skills () {
+            return View();
+        }
+
+        public IActionResult About () {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
